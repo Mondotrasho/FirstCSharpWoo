e8bc005280dcbddd5174f2a757d8959a68f01d99
Mondotrasho <oscar@peoplepictures.com.au>
2018-07-18 19:01:09 +1000

    Woo started

---
+++ FirstCSharpWoo/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSharpWoo
+{
+    class MyClass
+    {
+        public int SimpleAdditionFunction(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int SimpleSubtractionFunction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int SimpleMultiplicationFunction(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int SimpleDivisionFunction(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+
+
+    }
+}
--- FirstCSharpWoo/Program.cs
+++ FirstCSharpWoo/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace FirstCSharpWoo
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+            MyClass myClass = new MyClass();
+
+            int answer = myClass.SimpleAdditionFunction(3, 4);
+
+            //|||||||||
+
+            Console.WriteLine("Whats your name");
+           
+            string  word = Console.ReadLine();
+          
+
+            Console.WriteLine("Hello "+ word +" world");
+            while (true) ;
         }
+
+       
     }
 }
